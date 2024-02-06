24a99d521991f51a192211a8a4c664bf3ea54534
Nke0628 <ooo_diva@yahoo.co.jp>
2024-02-06 20:10:57 +0900

    feat: clamavの適用

--- lib/cdk-clamav-stack.ts
+++ lib/cdk-clamav-stack.ts
@@ -1,16 +1,20 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { ServerlessClamscan } from "cdk-serverless-clamscan";
+import { Bucket } from "aws-cdk-lib/aws-s3";
 
 export class CdkClamavStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkClamavQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const clamScan = new ServerlessClamscan(this, "rClamscan", {});
+    const bucket = new Bucket(this, "rBucket", {
+      autoDeleteObjects: true,
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
+    clamScan.addSourceBucket(bucket);
+    new cdk.CfnOutput(this, "oBucketName", {
+      description: "The name of the input S3 Bucket",
+      value: bucket.bucketName,
+    });
   }
 }
